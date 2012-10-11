80d210a424c5422b384097d94539f2bd11c0a991
MunkiPhD <munkiphd@gmail.com>
2012-10-11 11:04:47 -0400

    - modified .gitignore - initial app setup - added gems - setup rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Konvo::Application.config.secret_token = 'cb704202dc275d082a0357ee286e77c741771da8b0a174c9ccaed10e8a3165270480e1cbc9cd0cde6efe7ce0eaa71315d76e6a849e98dad9c948b97f02337923'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Konvo::Application.config.session_store :cookie_store, key: '_konvo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Konvo::Application.config.session_store :active_record_store
